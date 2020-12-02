2f46b45f35be9e3480cedb7e08363118fdfb4b6d
Kyle Hooks <kthprog@gmail.com>
2020-12-02 08:46:08 -0500

    Factory method WIP

---
+++ DesignPatterns/DesignPatterns.Patterns/FactoryMethod.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace DesignPatterns.Patterns
+{
+    public interface IFactoryMethod
+    {
+
+    }
+    public class FactoryMethod : IFactoryMethod
+    {
+    }
+}
